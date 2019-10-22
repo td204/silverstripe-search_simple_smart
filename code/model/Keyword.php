6433525b8c2902046ae147b6969b1dcb63ff3b13
terry <terry@td204.com>
2019-10-22 16:46:16 +0200

    Check for existing table on flush

--- code/model/Keyword.php
+++ code/model/Keyword.php
@@ -133,35 +133,38 @@ class SearchEngineKeyword extends DataObject implements Flushable {
 		return "#{$this->ID}";
 	}
 
-	public static function export_keyword_list() {
-		$fileName = self::get_js_keyword_file_name(true);
-		if($fileName) {
-			//only write once a minute
-			if(file_exists($fileName) && (time() -  filemtime($fileName) < 120) ) {
-				return "no new file created as the current one is less than 120 seconds old.";
-				//do nothing
-			}
-			else {
-				$rows = DB::query("SELECT \"Keyword\" FROM \"SearchEngineKeyword\" ORDER BY \"Keyword\";");
-				$array = array();
-				foreach($rows as $row) {
-					$array[] = str_replace('"', "", Convert::raw2js($row["Keyword"]));
-				}
-				$written = null;
-				if($fh = fopen($fileName, 'w')) {
-					$written = fwrite($fh, "SearchEngineInitFunctions.keywordList = [\"".implode("\",\"", $array)."\"];");
-					fclose($fh);
-				}
-				if(!$written) {
-					user_error("Could not write keyword list to $fileName", E_USER_NOTICE);
-				}
-				return "Writting: <br />".implode("<br />", $array);
-			}
-		}
-		else {
-			return "no file name specified";
-		}
-	}
+    public static function export_keyword_list() {
+        $fileName = self::get_js_keyword_file_name(true);
+        if($fileName) {
+            //only write once a minute
+            if(file_exists($fileName) && (time() -  filemtime($fileName) < 120) ) {
+                return "no new file created as the current one is less than 120 seconds old.";
+                //do nothing
+            } else {
+                $connection = DB::get_conn();
+                $schema = $connection->getSchemaManager();
+                if ($schema->hasTable('SearchEngineKeyword')) {
+                    $rows = DB::query("SELECT \"Keyword\" FROM \"SearchEngineKeyword\" ORDER BY \"Keyword\";");
+                    $array = array();
+                    foreach($rows as $row) {
+                        $array[] = str_replace('"', "", Convert::raw2js($row["Keyword"]));
+                    }
+                    $written = null;
+                    if($fh = fopen($fileName, 'w')) {
+                        $written = fwrite($fh, "SearchEngineInitFunctions.keywordList = [\"".implode("\",\"", $array)."\"];");
+                        fclose($fh);
+                    }
+                    if(!$written) {
+                        user_error("Could not write keyword list to $fileName", E_USER_NOTICE);
+                    }
+                    return 'Writing: <br />' .implode('<br />', $array);
+                }
+            }
+        }
+        else {
+            return 'no file name specified';
+        }
+    }
 
 	/**
 	 * returns the location of the keyword file...
